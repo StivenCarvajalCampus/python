c9497214cd7765746a003f35e08015eca270f479
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-02-07 09:11:51 -0500

    ejercicio7full

---
+++ SkillReview/ejercicio7.py
@@ -0,0 +1,16 @@
+""" 7. Cuál es la diferencia entre un condicional simple y un
+condicional compuesto? """
+""" un condicional simple evalua una condicion si se cumple imprime algo si no termina el codigo """
+""" un condicional compuesto evalua una condicion y puede tomar decisiones si se cumple con la condicion o si no lo cual puede llecar a terminar en condicionales anidadas. """
+#Condicional simple
+sueldo=int (input("Ingrese cual es su sueldo:"))
+if sueldo>3000:
+    print("Esta persona debe abonar impuestos")
+#CondicionalCompuesto
+""" num1=int(input("Ingrese primer valor:"))
+num2=int(input("ingrese segundo valor:"))
+print("El valor mayor es")
+if num1>num2:
+    print(num1)
+else:
+    print(num2) """
