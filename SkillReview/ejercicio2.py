cb8385203cedd8d3f6b39d73e6bba8feefaeaec0
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-02-07 07:41:03 -0500

    ejercicio2full

---
+++ SkillReview/ejercicio2.py
@@ -0,0 +1,9 @@
+""" 2. En la jerarquía de operadores, cuáles son los que más
+prioridad tienen cuando el intérprete de Python los evalúa? """
+num1= 4
+num2=8
+print("prioridad 1 Operaciones entre paréntesis ()\n prioridad 2 potencia ** \n ")
+
+print("prioridad 3 Multiplicación y División, módulo o residuo División entera *,/,%,// \n prioridad 4 suma y resta +,-\n prioridad 5 operadores relacionales <, <=, >, >=, !=, ==\n")
+
+print("prioridad 6 operador logico AND \n Operador logico OR")
