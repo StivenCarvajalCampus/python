604171ff8f3126f81e8f1cd74adae8420ea0291d
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-02-07 09:00:16 -0500

    ejercicio6full

---
+++ SkillReview/ejercicio6.py
@@ -0,0 +1,8 @@
+""" 6. En sus propias palabras, qué son las funciones
+preconstruidas y proporcione 2 ejemplos. """
+""" Una función es un bloque de código, que recibe cero o más argumentos y que una operación para devolvernos un valor y/o realiza una tarea específica.  """
+nombre= "stiven".upper()
+
+print(list (range(10)))
+print(nombre)
+
