1a63f5c6b5670f387a3a92f1c8c758fdb0ff7015
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-02-07 08:11:55 -0500

    ejercicio4

---
+++ SkillReview/ejercicio4.py
@@ -0,0 +1,2 @@
+""" 4. Que son las expresiones regulares en Python? """
+""" Las expresiones regulares (llamadas RE, o regex, o patrones de regex) son esencialmente en un lenguaje de programación diminuto y altamente especializado incrustado dentro de Python y disponible a través del módulo re. Usando este pequeño lenguaje, especificas las reglas para el conjunto de cadenas de caracteres posibles que deseas hacer coincidir. """
