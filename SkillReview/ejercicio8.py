dc3a91371d5aa82c6a568583205a6b87dca9d06e
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-02-07 09:18:47 -0500

    ejercicio8full

---
+++ SkillReview/ejercicio8.py
@@ -0,0 +1,11 @@
+""" 8. Escriba un bloque cualquiera de código en Python en donde
+utilice 2 condicionales (if) anidados. """
+num1=int(input("Ingrese primer valor:"))
+num2=int(input("ingrese segundo valor:"))
+num3=int(input("ingrese tercer valor:"))
+print("El valor mayor es")
+if num1>num2:
+    print(num1)
+else:
+    if num2>num3:
+     print(num3) 
