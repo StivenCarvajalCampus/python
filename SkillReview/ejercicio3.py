97bb77c55a8029b2d3fe33fb936974201481c0b1
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-02-07 07:52:32 -0500

    ejercicio3full

---
+++ SkillReview/ejercicio3.py
@@ -0,0 +1,8 @@
+""" Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
+
+A. De izquierda a derecha
+B. De derecha a izquierda """
+num1= 8
+num2=9
+print(f"A. De izquierda a derecha \n{num1} + {num2} * {num1} // {num1} = {num1+num2*num1//num1}")
