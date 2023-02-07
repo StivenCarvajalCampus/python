3cd25033d5737913806c42a9067f0c3963a30c6d
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-02-07 08:39:04 -0500

    ejercicio5full

---
+++ SkillReview/ejercicio5.py
@@ -0,0 +1,10 @@
+""" 5. Enumere 5 tipos de datos en Python y suministre un valor de
+ejemplo de cada uno. """
+num= 2 #int
+long= 456966786151987643
+float= 3.1415927
+Hola= 'hola'#string
+ropa= True
+list= ["camiseta","jean","gorro"]
+
+print(f"int {num}\n long {long}\n float {float}\n string {Hola}\n Booleano {ropa}\n Lista {list} ")
