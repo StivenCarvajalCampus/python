b8e0f7b207e352ec3b6dd048392aff487eb07b5a
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-02-07 07:28:38 -0500

    ejercicio1 full

--- SkillReview/ejercicio1.py
+++ SkillReview/ejercicio1.py
@@ -2,3 +2,12 @@
 A. División Modular: %
 B. Exponenciación: **
 C. División que retorne entero: // """
+
+num1=2
+num2=4
+
+print(f"A.Division modular: % \n{num1} % {num2} {num1%num2}")
+
+print(f"B.Exponenciacion: ** \n{num1} ** {num2} {num1**num2} ")
+
+print(f"C.Division que retorne entero: // \n {num1} // {num2} {num1//num2}")
